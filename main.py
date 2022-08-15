c34d71fb8599905602f5b1cbfe48eee447a9ad48
HogRider <danilzzzg19@gmail.com>
2022-08-16 00:54:40 +1000

    Added content_filter function!

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ def get_json(url, params = None):
         print(f'[INFO]: HTTP error: {http_err}')
 
 
+def content_filter(content_page):
+    """Отбирает определенные поля из словаря с вакансиями"""
+    required_fields = ['id', 'name', 'area', 'salary', 'address', 'published_at', 'url', 'employer', 'snippet']
+
+    filter_content_page = []
+    for content in content_page:
+        filter_content = {}
+        for required_field in required_fields:
+            filter_content[required_field] = content[required_field]
+
+        filter_content_page.append(filter_content)
+    
+    return filter_content_page
+
+
 def get_content(vacancy_name, num_page=1):
     """Получение списка словарей со всеми вакансиями"""
 
@@ -29,14 +44,17 @@ def get_content(vacancy_name, num_page=1):
         next_page_content = get_json(MAIN_URL+'vacancies/', params)
 
         if next_page_content:
-            content += next_page_content
+            content += content_filter(next_page_content)
             print(f'[INFO]: Обработанно {page_index+1} из {num_page} страниц')
+
+    print(f'\n[INFO]: Всего собрано {len(content)} вакансий\n')
 
     return content
 
 
+
 def main():
-    print(len(get_content('Переводчик', 3)))
+    print(get_content('Python develper', 3)[0])
 
 
 if __name__ == '__main__':
