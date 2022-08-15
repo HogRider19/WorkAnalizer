7269a17e9bc1f15aacbfab40f49720836f4ca468
HogRider <danilzzzg19@gmail.com>
2022-08-15 23:46:08 +1000

    Added get_content and get_json functions!

--- main.py
+++ main.py
@@ -1,9 +1,42 @@
+import csv
 import requests
-from fake_useragent import FakeUserAgent
+import json
+from fake_useragent import UserAgent
+from requests import HTTPError
+
+MAIN_URL = 'https://api.hh.ru/'
+HEADERS = {'User-Agent': UserAgent().chrome}
+
+
+def get_json(url, params = None):
+    """Запрос json файла с вакансиями"""
+    try:
+        response = requests.get(url, params=params, headers=HEADERS)
+        response.raise_for_status()
+        return response.json()['items']
+    except HTTPError as http_err:
+        print(f'[INFO]: HTTP error: {http_err}')
+
+
+def get_content(vacancy_name, num_page=1):
+    """Получение списка словарей со всеми вакансиями"""
+
+    params = {'text': vacancy_name, 'per_page': 100}
+    content = []
+
+    for page_index in range(num_page):
+        params['page'] = page_index
+        next_page_content = get_json(MAIN_URL+'vacancies/', params)
+
+        if next_page_content:
+            content += next_page_content
+            print(f'[INFO]: Обработанно {page_index+1} из {num_page} страниц')
+
+    return content
 
 
 def main():
-    pass
+    print(len(get_content('Переводчик', 3)))
 
 
 if __name__ == '__main__':
